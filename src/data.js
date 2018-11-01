baa629074285e8fc7dd725d4f4338fa7ddd8941a
EthanFe <sneakcow@gmail.com>
2018-11-01 17:09:56 -0500

    add burj khalifa for some reason

--- src/data.js
+++ src/data.js
@@ -2,7 +2,8 @@ import {index} from "./funtimes"
 
 export const buildingTypes = [ 
   { name: "lumber mill", image: "mill", cost: {stone: 40, wood: 10}},
-  { name: "mine", image: "mine", cost: {stone: 15, wood: 35}}
+  { name: "mine", image: "mine", cost: {stone: 15, wood: 35}},
+  { name: "burjkhalifa", image: "burjkhalifa", cost: {stone: 1000000, wood: 1000000}}
 ]
 
 export const buildingTypesByName = index(buildingTypes, 'name')
