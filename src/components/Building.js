c95ae1f74f30fb99e2b578acef4f07c1f01db17e
EthanFe <sneakcow@gmail.com>
2018-11-01 16:53:26 -0500

    gray out buildings when player doesnt have enough resources for them

--- src/components/Building.js
+++ src/components/Building.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import {buildingTypesByName} from '../data'
+import {index} from '../funtimes'
 
 class Building extends Component {
   render() {
     let image = buildingTypesByName[this.props.type].image
-    return <img className="building-icon" src={require(`../images/${image}.png`)} alt=""/>
+    let className = "building-icon"
+    if (this.isBuildable(this.props.type))
+      className += " buildable"
+    return <img className={className} src={require(`../images/${image}.png`)} alt=""/>
+  }
+
+  isBuildable = (buildingType) => {
+    const costs = buildingTypesByName[buildingType].cost
+    for (const resource in costs) {
+      const currentResourceAmount = index(this.props.resources, "type")[resource].amount
+      if (currentResourceAmount < costs[resource])
+        return false
+    }
+    return true
   }
 }
 
